8f3a89b398bc79d5af3e84d27cb119537e531b55
3dprogramin <admin@3dprogramin.com>
2019-03-12 19:33:37 +0200

    added compression

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const format = require('string-format');
+const compression = require('compression');
 require('colors');
 
 var indexRouter = require('./routes/index');
@@ -19,6 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(compression());   // gzip
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
